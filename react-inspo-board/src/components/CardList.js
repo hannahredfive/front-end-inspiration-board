4ba1ce3902dd7d7fc88fb361b36e4330a977f855
mlhuynh <greenlinh@gmail.com>
2023-06-28 15:40:51 -0700

    Completed CardList function and defined PropTypes in CardList component.

--- react-inspo-board/src/components/CardList.js
+++ react-inspo-board/src/components/CardList.js
@@ -1,20 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Card from './Card';
-import NewCardForm from './NewBoardForm';
+// import NewCardForm from './NewBoardForm';
 import './CardList.css';
 
+// Reminder: Update props with {currentBoard, cards, incrementLikeCount, onDeleteCard, deleteBoard}
 const CardList = (props) => {
     return (
         <section>
-            <Card />
-            <NewCardForm />
+          {props.cardsData.map((card) => (
+            <Card
+              key={`${card.id}-${card.boardID}`}
+              cardID={card.id}
+            //   boardID={card.boardID}
+              likeCount={card.likeCount}
+              message={card.message}
+              likeCard={props.likeCard}
+              deleteCard={props.deleteCard}
+            />
+          ))}
         </section>
-    )
-};
+      );
+    };
 
 CardList.propTypes = {
-
+    cardsData: PropTypes.arrayOf(
+        PropTypes.shape({
+            cardID: PropTypes.number,
+            // boardID: PropTypes.number.isRequired,
+            likeCount: PropTypes.number,
+            message: PropTypes.string
+        })
+    )
 };
 
 export default CardList;
