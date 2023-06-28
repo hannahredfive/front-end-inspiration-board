2751ae41c0d89235db911438276d0e9051fc5598
Hannah <hannah@chandleys.com>
2023-06-28 10:32:41 -0700

    Renamed import to prevent bug

--- react-inspo-board/src/App.js
+++ react-inspo-board/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import CardList from './components/TaskList.js';
+import CardList from './components/CardList.js';
 import BoardList from './components/BoardList.js';
 import './App.css';
 import axios, {isCancel, AxiosError} from 'axios';
