a584752ed1a8e8219d4e9a52f7f5605f9c76702d
mlhuynh <greenlinh@gmail.com>
2023-06-28 11:50:47 -0700

    Implemented onClickLikeButton function in Card component.

--- react-inspo-board/src/components/Card.js
+++ react-inspo-board/src/components/Card.js
@@ -2,16 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Card.css';
 
+
 const Card = (props) => {
+
+    const onClickLikeButton () => {
+        console.log('Like Button working')
+        props.onClickCallback(props.id);
+    };
+
     return (
         <section>
-            Blah!
+            <p>'blah!'</p>
         </section>
     )
-};
-
-Card.propTypes = {
-
 };
 
 export default Card;
