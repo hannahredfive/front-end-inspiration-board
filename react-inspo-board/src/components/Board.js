227e4f0fcaf66a6d6dcc6776c8d361e4eb8e6a8e
Hannah <hannah@chandleys.com>
2023-06-28 11:49:44 -0700

    Completed initial rendering for Board that will render the cardlist based on the board selected

--- react-inspo-board/src/components/Board.js
+++ react-inspo-board/src/components/Board.js
@@ -4,15 +4,21 @@ import './Board.css';
 import CardList from './CardList';
 
 const Board = (props) => {
-    return (
-        <section>
-            <CardList />
-        </section>
-    )
+  return (
+    <section>
+      <h2>{props.title}</h2>
+      <h4>{props.owner}</h4>
+      <span className='card__list'>
+        <CardList boardId={props.id} />
+      </span>
+    </section>
+  )
 };
 
 Board.propTypes = {
-
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    owner: PropTypes.string.isRequired
 };
 
 export default Board;
