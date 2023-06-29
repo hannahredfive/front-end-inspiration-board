fbf5b697df6118fa38de3ff54c91d1472255ce9f
mlhuynh <greenlinh@gmail.com>
2023-06-29 08:22:28 -0700

    Created sample card data in App component.

--- react-inspo-board/src/App.js
+++ react-inspo-board/src/App.js
@@ -4,7 +4,24 @@ import BoardList from './components/BoardList.js';
 import './App.css';
 import axios, {isCancel, AxiosError} from 'axios';
 
+
 function App() {
+
+  const [cardData, setCardData] = useState([
+    {
+      id: 1,
+      message: 'message 1'
+    },
+    {
+      id: 2,
+      message: 'message 2'
+    },
+    {
+      id: 3,
+      message: 'message 3'
+    }
+  ]);
+
   return (
     <div>
       <header>
@@ -13,6 +30,9 @@ function App() {
       </header>
       <main>
         <BoardList />
+        <CardList 
+          cards={cardData}
+        />
       </main>
     </div>
   );
