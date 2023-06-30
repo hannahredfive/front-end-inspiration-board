0dccdd286b878159b8ded1580a965adfdd3a36fd
mlhuynh <greenlinh@gmail.com>
2023-06-30 12:32:11 -0700

    Relocated Card state and functions from App to Board component.

--- react-inspo-board/src/components/Board.js
+++ react-inspo-board/src/components/Board.js
@@ -4,6 +4,87 @@ import './Board.css';
 import CardList from './CardList';
 
 const Board = (props) => {
+
+  const URL_PREFIX = 'https://back-end-inspiration-board.onrender.com';
+  const [cardData, setCardData] = useState([]);
+
+  //Get API calls from axios for rendering all cards
+  const getAllCards = (boardId) => {
+    axios
+      .get(`${URL_PREFIX}/boards/${boardId}/cards`)
+      .then((response) => {
+        console.log(response.data);
+        setCardData(response.data.cards);
+      })
+      .catch((error) => {
+        console.error(error.response.data.message);
+      });
+  };
+
+  const likeCard = (cardId) => {
+    axios
+      .patch(`${URL_PREFIX}/cards/${cardId}`)
+      .then((result) => {
+        const newCards = [...cardData];
+        for (const card of newCards) {
+          if (card.id === cardId) {
+            console.log(result);
+            card.likes_count = result.data.likes_count;
+          }
+        }
+        setCardData(newCards);
+      })
+      .catch((error) => {
+        console.error(error.response.data.message);
+      });
+  };
+
+  const deleteCard = (cardId) => {
+    axios
+      .delete(`${URL_PREFIX}/cards/${cardId}`)
+      .then(() => {
+        const newCards = cardData.filter((card) => card.id !== cardId);
+        setCardData(newCards);
+      })
+      .catch((error) => {
+        console.error(error.response.data.message);
+      });
+  };
+
+  // Reminder: Check with Hannah regarding name of board id and select board *object* function
+  const addCard = (message) => {
+    axios
+      .post(
+        `${URL_PREFIX}/boards/${currentBoard.board_id}/cards`,
+        { message }
+      )
+      .then((result) => {
+        console.log(result);
+        const newCard = {
+          id: result.data.card.id,
+          message: result.data.card.message,
+          likes_count: result.data.card.likes_count,
+          // boardId: result.data.card.board_id,
+        };
+        setCardData([...cardData, newCard], () => {
+          console.log(this.state.cardsData);
+        });
+      })
+      .catch((error) => console.log(error.response.data));
+  };
+
+  const deleteCard = (cardId) => {
+    axios
+      .delete(`${URL_PREFIX}/cards/${cardId}`)
+      .then(() => {
+        const newCards = cardData.filter((card) => card.id !== cardId);
+        setCardData(newCards);
+      })
+      .catch((error) => {
+        console.error(error.response.data.message);
+      });
+  };
+
   return (
     <section>
       <h2>{props.title}</h2>
